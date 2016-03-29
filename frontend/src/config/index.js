80ae82b570f2e4f718f0677f5714c0f8dbafd42d
dustin-H <dustin.hoffner@gmail.com>
2016-03-29 20:31:58 +0200

    Restructure config

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,27 +1,13 @@
 
-var fs = require('fs')
-var file = 'config.json';
-
-var m = {};
-
-var config = JSON.parse(fs.readFileSync(file, 'utf8'));
-
-var update = function(newConfig, cb) {
-  var temp = Object.assign(config, newConfig)
-  fs.writeFile(file, JSON.stringify(temp), function(err) {
-    if (err != null) {
-      return cb(err)
-    }
-    fs.readFile(file, 'utf8', function(err, latestConfig) {
-      if (err != null) {
-        return cb(err)
-      }
-      config = Object.assign(config, JSON.parse(latestConfig))
-      cb(null);
-    })
-  })
+var config = {
+	"routes": [],
+	"theme": "default",
+	"address": {
+		"api": "http://localhost:8000/",
+		"own": "http://localhost:8000/"
+	},
+	"errors": []
 }
 
-config.update = update;
 
-module.exports = config
+export default config
