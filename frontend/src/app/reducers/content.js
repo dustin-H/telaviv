b7ac0898f4accb4ab26855e577fbc8e25d97b33d
dustin-H <dustin.hoffner@gmail.com>
2016-03-29 20:26:29 +0200

    Action reducers

--- frontend/src/app/reducers/content.js
+++ frontend/src/app/reducers/content.js
@@ -1,14 +1,20 @@
-import * as types from '../constants/ActionTypes.js';
+import * as types from '../constants/ActionTypes.js'
 
-const initialState = {data: []};
+const initialState = {data: [], routes: {}} // This will never be used, because the state gets initialized by bauhaus before
 
 export default function content(state = initialState, action) {
   switch (action.type) {
-    case types.EXAMPLE_SYNC:
-      var newState = Object.assign({}, state);
-      newState.example += action.data;
-      return newState;
+    case types.CONTENT_LOCATION_CHANGED:
+      var newState = Object.assign({}, state)
+      newState.location = action.location
+      newState.data = action.data
+      newState.loading = false
+      return newState
+    case types.CONTENT_SET_LOADING:
+      var newState = Object.assign({}, state)
+      newState.loading = true
+      return newState
     default:
-      return state;
+      return state
   }
 }
