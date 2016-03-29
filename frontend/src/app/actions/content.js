18616a5fb48dbfc7f2d5a93146a54c13764d6007
dustin-H <dustin.hoffner@gmail.com>
2016-03-29 20:14:41 +0200

    Routing actions for client side app

--- frontend/src/app/actions/content.js
+++ frontend/src/app/actions/content.js
@@ -1,29 +1,59 @@
 import * as types from '../constants/ActionTypes.js';
 
-import matchRoutes from '../../router/matchRoutes.js'
+import contentGenerator from '../contentGenerator'
 import isServer from '../../utils/isServer.js'
 
 import { get } from '../store/store.js'
 
-import { createHistory } from 'history/lib/createBrowserHistory'
+import createHistory from 'history/lib/createBrowserHistory'
+
+var history = null
+
 if (isServer !== true) {
-  let history = createHistory()
+  history = createHistory()
   let unlisten = history.listen(location => {
-    get().dispatch(locationChanged(location))
+    //get().dispatch(locationChanged(location))
   })
 
-  function locationChanged(location) {
-    return (dispatch, getState) => {
-      var state = getState()
-      var route = matchRoutes(state.content.routes, location)
+  history.listenBefore(function(location, callback) {
+     get().dispatch(locationChanged(location, callback))
+  })
 
-      dispatch({
-        type: types.CONTENT_LOCATION_CHANGED,
-        location,
-        route
+  function locationChanged(location, callback) {
+    return (dispatch, getState) => {
+
+      dispatch(setLoading())
+      var state = getState()
+
+      contentGenerator(location, state.config.routes, (err, data) => {
+        if (err != null) {
+          var href = history.createHref(location)
+          console.log(href);
+          // window.location.replace(href);
+          callback(false)
+          window.location.href = href
+          /*unlisten()
+          window.onpopstate = function(){
+            window.location.href = href
+          }
+          window.history.go(-1)*/
+
+          return
+        }
+
+        callback(true)
+        dispatch({
+          type: types.CONTENT_LOCATION_CHANGED,
+          location,
+          data
+        })
       })
     }
   }
+}
+
+function setLoading() {
+  return {type: types.CONTENT_SET_LOADING}
 }
 
 export function pushLocation(location) {
