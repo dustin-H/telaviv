6a3a941e784e65a4941bd54ec40048752e9f7756
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:41:24 +0200

    Package client/server switch fix

--- frontend/src/app/loader/index.js
+++ frontend/src/app/loader/index.js
@@ -1,8 +1,13 @@
 
 import config from '../../config'
+import { setClassNameScope, getClassNameScope } from 'react-look-scope'
 
 export function getComponent(id) {
-  return require('../../themes/' + config.theme + '/' + id)
+  let oldscope = getClassNameScope()
+  setClassNameScope(config.components[id])
+  let temp = require('../../themes/' + config.theme + '/' + id)
+  setClassNameScope(oldscope)
+  return temp
 }
 
 export function ensureComponents(ids, cb) {
