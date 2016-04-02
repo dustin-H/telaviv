10db03639bd904cc1e5596c69622274fa898a2ec
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:40:33 +0200

    Refactor: Middleware flow

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,11 +7,10 @@ import redirect from './middleware/redirect.js'
 import prepareRouteFetch from './middleware/prepareRouteFetch.js'
 import fetch from './middleware/fetch.js'
 import renderReact from './middleware/renderReact.js'
-import generateHtml from './middleware/generateHtml.js'
+import generateHtml from './middleware/generateHtml'
 import respond from './middleware/respond.js'
 
 import prepareErrorFetch from './middleware/prepareErrorFetch.js'
-import catchErrors from './middleware/catchErrors.js'
 
 import config from './config'
 import setConfig from './config/setConfig.js'
@@ -30,7 +29,16 @@ module.exports = function(c) {
   setConfig(c)
   var app = express()
 
-  app.use('/.static', staticProvider())
+  app.use((req, res, next) => {
+    /*for (var i in require.cache) {
+      if (i.search('/themes/') >= 0 || i.search('/node_modules/react-look/') >= 0) {
+        delete require.cache[i]
+      }
+    }*/
+    next()
+  })
+
+  app.use('/.static', staticProvider(config))
 
   app.use(init(config))
   app.use(router(config))
@@ -45,7 +53,7 @@ module.exports = function(c) {
   app.use(errorHandler(prepareErrorFetch(config)))
   app.use(errorHandler(fetch(config)))
   app.use(errorHandler(renderReact(config)))
-  app.use(errorHandler(catchErrors(config)))
+  // app.use(errorHandler(catchErrors(config)))
   app.use(errorHandler(generateHtml(config)))
   app.use(errorHandler(respond(config)))
 
