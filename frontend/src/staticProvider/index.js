afff849475cbbb44390d3c148f82d2f436d5f595
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:45:35 +0200

    Moved config of static Provider to params

--- frontend/src/staticProvider/index.js
+++ frontend/src/staticProvider/index.js
@@ -1,19 +1,12 @@
 
 import express from 'express'
-import config from '../config'
 
-export default () => {
+export default (config) => {
 
   var app = express()
 
   var bundlePath = require.resolve('../../client-build/bundle.js')
   var themesPath = bundlePath.split('bundle.js')[0] + 'themes/'
-
-  /*app.use((req, res, next) => {
-    setTimeout(() => {
-      next()
-    }, 500)
-  })*/
 
   app.use('/bundle.js', express.static(bundlePath))
 
