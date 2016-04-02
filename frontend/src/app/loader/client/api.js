1b26086b558fc37e14a2af45bed34d3ae996ff8a
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:41:42 +0200

    Add react-look-scope to client preinstalled dependencies

--- frontend/src/app/loader/client/api.js
+++ frontend/src/app/loader/client/api.js
@@ -3,7 +3,8 @@ __GLOBAL__.npm = {
   react: require('react'),
   'react-dom': require('react-dom'),
   superagent: require('superagent'),
-  'react-look': require('react-look')
+  'react-look': require('react-look'),
+  'react-look-scope': require('react-look-scope')
 /*'react-lib-ReactTransitionGroup': require('react/lib/ReactTransitionGroup'),
 'react-lib-update': require('react/lib/update'),
 'react-lib-ReactComponentWithPureRenderMixin': require('react/lib/ReactComponentWithPureRenderMixin'),
