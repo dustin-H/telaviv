2dd61612826ffa9303841d22ce44545c42dd3c59
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:44:29 +0200

    Refactor renderReact middleware

--- frontend/src/middleware/renderReact.js
+++ frontend/src/middleware/renderReact.js
@@ -1,17 +1,18 @@
 
 import renderReact from '../app/server.js'
 
-export default function() {
+export default function(config) {
   return (req, res, next) => {
     req.bauhaus.renderData = null
     if (req.bauhaus.fetch == null || req.bauhaus.fetch.data == null) {
-      return next(true)
+      return next(500)
     }
-    let renderData = renderReact(req.bauhaus.fetch.data, req, res)
-    if (renderData != null) {
-      req.bauhaus.renderData = renderData
-      return next()
+    let renderData = renderReact(req.bauhaus.fetch.data, req, res, config)
+    if (renderData == null) {
+      // When renderData is null some react component has redirected the request
+      return
     }
-    next(true)
+    req.bauhaus.renderData = renderData
+    next()
   }
 }
