2a1aeb8b4045275958b205d83fbdc566264d1bfd
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 17:44:43 +0200

    Refactor router middleware

--- frontend/src/middleware/router.js
+++ frontend/src/middleware/router.js
@@ -5,7 +5,6 @@ export default function(config) {
   return (req, res, next) => {
     req.bauhaus.route = matchRoutes(config.routes, req.path)
     if (req.bauhaus.route === false) {
-      res.status(404)
       return next(404)
     }
     next()
