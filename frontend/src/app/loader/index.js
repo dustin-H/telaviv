243f6c7b1022d744188b00c2ab294adca86ce336
dustin-H <dustin.hoffner@gmail.com>
2016-04-02 18:03:41 +0200

    Removed never used functions

--- frontend/src/app/loader/index.js
+++ frontend/src/app/loader/index.js
@@ -9,11 +9,3 @@ export function getComponent(id) {
   setClassNameScope(oldscope)
   return temp
 }
-
-export function ensureComponents(ids, cb) {
-  throw new Error('Do not call loader/ensureComponents on the server-side!')
-}
-
-export function registerComponent(id, module) {
-  throw new Error('Do not call loader/registerComponent on the server-side!')
-}
