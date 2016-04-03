0eb36e83035a0b01bd689ee1a6e82c66064e9510
dustin-H <dustin.hoffner@gmail.com>
2016-04-03 12:10:56 +0200

    New react test

--- frontend/test/middleware/renderReact.js
+++ frontend/test/middleware/renderReact.js
@@ -9,17 +9,41 @@ describe('Using the init middleware', () => {
   it('should reject with code 500 when fetch or data is null', () => {
     let mid = renderReact({})
 
-    var req = {
+    let req = {
       bauhaus: {
         fetch: {}
       }
     }
-    var res = {}
+    let res = {}
     const next = sinon.spy()
-
     mid(req, res, next)
 
     expect(next).to.have.been.calledOnce
     expect(next).to.have.been.calledWith(500)
+  })
+  it('should render a data component including the correct data', () => {
+    let mid = renderReact(TestUtils.mockRenderReactConfig())
+
+    let req = {
+      bauhaus: {
+        fetch: {
+          data: [{
+            component: 'Data',
+            data: {
+              some: 'data'
+            }
+          }]
+        }
+      },
+      path: '/some/path'
+    }
+    let res = {redirect: sinon.spy()}
+    const next = sinon.spy()
+    mid(req, res, next)
+
+    expect(res.redirect).to.have.been.calledNever
+    expect(next).to.have.been.calledOnce
+    expect(next.args[0].length).to.equal(0)
+    console.log(JSON.stringify(req.bauhaus.renderData, null, 2));
   })
 })
