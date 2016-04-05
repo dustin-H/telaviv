eb54bddc0b368b6eb94814b0bc575eec7035265f
dustin-H <dustin.hoffner@gmail.com>
2016-04-05 15:55:08 +0200

    Add and provide telaviv icon

--- src/staticProvider/index.js
+++ src/staticProvider/index.js
@@ -7,10 +7,13 @@ export default (config) => {
 
   var bundlePath = require.resolve('../../client-build/bundle.js')
   var themesPath = bundlePath.split('bundle.js')[0] + 'themes/'
+  var telavivPath = require.resolve('../../img/icon_small.png')
 
   app.use('/bundle.js', express.static(bundlePath))
 
   app.use('/components', express.static(themesPath + config.theme + '/'))
+
+  app.use('/telaviv.png', express.static(telavivPath))
 
   return app
 
