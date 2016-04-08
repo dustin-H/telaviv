aa9739b4d115ec13d7519862f2b802fb22965800
dustin-H <dustin.hoffner@gmail.com>
2016-04-08 16:05:24 +0200

    Removed config set testing

--- app.js
+++ app.js
@@ -11,23 +11,27 @@ config.update({lala: 'Größenwahn!'}, function(err) {
 var compression = require('compression');
 var express = require('express')
 var app = express()
-//var morgan = require('morgan')
+// var morgan = require('morgan')
 
 var config = require('./config.json')
 
-var telaviv = require('./lib/app.js');
+var telaviv = require('./lib/index.js');
 var testapi = require('./testapi.js');
 
-app.listen(8000, function(){
+app.listen(8000, function() {
   console.log('STARTED!');
 })
 
-//app.use(morgan('combined'))
+// app.use(morgan('combined'))
 app.use(compression());
+app.get('/telaviv', function(req, res) {
+  telaviv.setConfig(config)
+  res.json(config)
+})
 app.use('/api', testapi())
 
-app.use(telaviv(config))
+app.use(telaviv.server(config))
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.status(500).send('telaviv Failed 500')
 })
