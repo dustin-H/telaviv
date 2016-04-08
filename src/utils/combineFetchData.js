25faa465eeb19a2b79795f6234ad6258e4023dd9
dustin-H <dustin.hoffner@gmail.com>
2016-04-08 11:13:47 +0200

    Same components with same data not shown bug fix

--- src/utils/combineFetchData.js
+++ src/utils/combineFetchData.js
@@ -3,13 +3,14 @@ import { objectReplacer } from './replace.js'
 
 export default (content, data, params) => {
   var out = [];
-  for(var i in content){
+  for (var i in content) {
     var obj = {};
     obj.component = content[i].component
+    obj.i = i
     obj.options = content[i].options || {}
     obj.options = JSON.parse(JSON.stringify(obj.options))
     obj.options = objectReplacer(obj.options, params)
-    obj.data = data[content[i].component] || {}
+    obj.data = data[content[i].component] || {}
     obj.key = generateObjectHash(obj)
     out.push(obj)
   }
