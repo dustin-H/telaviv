2aea746bbbb768bd2d75a5326287f84f26d14f44
dustin-H <dustin.hoffner@gmail.com>
2016-04-08 11:12:54 +0200

    Service unavailable handler

--- src/middleware/serviceUnavailable.js
+++ src/middleware/serviceUnavailable.js
@@ -1,6 +1,9 @@
 
 export default function() {
-  return (req, res, next) => {
+  return (req, res, next, error) => {
     res.status(503).send('<html><body style="text-align: center; font-family: Helvetica; margin-top: 100px; font-size: 40px;">Service Unavailable<br/><br/><span style="font-size: 16px;">(ERROR 503)</span><br/><br/><span style="font-size: 16px;">Tel-Aviv.JS totally crashed!<br/><br/><img src="/.static/telaviv.png" width="80" alt="TelAviv.JS" /></span></body></html>')
+    if(error.stack != null){
+      return console.error(error.stack)
+    }
   }
 }
--- test/middleware/serviceUnavailable.js
+++ test/middleware/serviceUnavailable.js
@@ -26,7 +26,36 @@ describe('Using the serviceUnavailable middleware', () => {
 
     const next = sinon.spy()
 
-    mid(null, res, next)
+    mid(null, res, next, {stack: 'MyTestStackTrace'})
+
+    expect(status).to.have.been.calledOnce
+    expect(status).to.have.been.calledWith(503)
+    expect(send).to.have.been.calledOnce
+    expect(send.args[0][0]).to.be.a('string')
+    expect(send.args[0][0].length).to.be.within(10, Infinity)
+    expect(next).to.have.been.calledNever
+  })
+  it('should return a function that calls res.send with the correct content without stack trace', () => {
+    let mid = serviceUnavailable()
+
+    var send = sinon.spy()
+    var status = sinon.spy()
+
+    var res = {}
+
+    res.send = (data) => {
+      send(data)
+      return res
+    }
+
+    res.status = (data) => {
+      status(data)
+      return res
+    }
+
+    const next = sinon.spy()
+
+    mid(null, res, next, {})
 
     expect(status).to.have.been.calledOnce
     expect(status).to.have.been.calledWith(503)
